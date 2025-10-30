f7f898464fe65a92b3414c454c987388471f3a0d
Kishore513313 <chk28555@gmail.com>
2025-10-31 01:36:51 +1300

    Initial framework setup for Mars Onboarding Project

---
+++ Drivers/Driver.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace MarsOnBoardingReqnRollProject.Drivers
+{
+    public class Driver
+    {
+        public static IWebDriver driver;
+
+        public static void Initialize()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        public static void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
+
