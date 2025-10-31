8e790c54685618d624c6534f36adaadb500478a9
Kishore513313 <chk28555@gmail.com>
2025-10-31 13:38:12 +1300

    Added hooks and updated project structure

---
+++ Hooks/Hooks.cs
@@ -0,0 +1,22 @@
+﻿using MarsOnBoardingReqnRollProject.Drivers;
+using Reqnroll;
+
+namespace Mars_Onboarding_Project.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            Driver.Initialize();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            Driver.Close();
+        }
+    }
+}
+
